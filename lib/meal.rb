33c85ad9e99590b0199c4309e26a3245e8e04ea9
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2021-01-10 22:47:14 -0500

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,27 @@
 class Meal
+    attr_accessor :waiter, :customer, :total, :tip
+  
+    @@all = []
+  
+    def initialize(waiter, customer, total, tip=0)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self
+    end
+  
+    def self.all
+        @@all
+    end
+end 
 
-end
+
+# Object relationships
+#   Customer
+#     #new_meal
+#       initializes a meal using the current Customer instance, a provided Waiter instance and a total and tip
+#     #meals
+#       returns an Array of Meal instances associated with this customer
+#     #waiters
+#       returns an Array of Waiter instances associated with this customer's meals
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,40 @@
 class Waiter
+    attr_accessor :name, :yrs_experience
+  
+    @@all = []
+  
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self
+    end
+  
+    def self.all
+        @@all
+    end
 
-end
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end 
+
+    def meals
+        Meal.all.select {|meal|meal.waiter == self}
+    end
+
+    def best_tipper
+        best_tipped_meal = meals.max {|meal_a, meal_b| meal_a.tip <=> meal_b.tip}
+        best_tipped_meal.customer
+      end 
+end 
+
+
+# Waiter
+#     #new_meal
+#       initializes a meal using the current Waiter instance, a provided Customer instance and a total and tip
+#     #meals
+#       returns an Array of Meal instances associated with this waiter
+#     #best_tipper
+#       returns the Customer instance associated with the meal that received the largest tip
+
+# Finished in 0.00742 seconds (files took 0.11442 seconds to load)
+# 12 examples, 0 failures
